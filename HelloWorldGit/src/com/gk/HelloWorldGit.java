562e7e4bf0597e82d7b8d0eaeabb15d37ae8f8a3
Gerry Kovan <gkovan@hotmail.com>
2017-06-12 19:32:38 -0400

    minor change to code.

--- HelloWorldGit/src/com/gk/HelloWorldGit.java
+++ HelloWorldGit/src/com/gk/HelloWorldGit.java
@@ -4,7 +4,7 @@ public class HelloWorldGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World Git v0.1 - initial version");
+		System.out.println("Hello World Git v0.2");
 
 	}
 
