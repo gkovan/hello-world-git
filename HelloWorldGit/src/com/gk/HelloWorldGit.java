9b47043183832940109c1fcb7966ea6e7a4e0b8d
Gerry Kovan <gkovan@hotmail.com>
2017-06-12 19:31:02 -0400

    Initial version of code.

---
+++ HelloWorldGit/src/com/gk/HelloWorldGit.java
@@ -0,0 +1,11 @@
+package com.gk;
+
+public class HelloWorldGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Git v0.1 - initial version");
+
+	}
+
+}
