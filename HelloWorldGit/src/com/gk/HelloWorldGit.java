ee29ec6945c8a46050e2ae0382d52aadb4ef2720
gkovan <gkovan@hotmail.com>
2017-07-30 20:00:33 -0400

    Update HelloWorldGit.java

--- HelloWorldGit/src/com/gk/HelloWorldGit.java
+++ HelloWorldGit/src/com/gk/HelloWorldGit.java
@@ -7,6 +7,7 @@ public class HelloWorldGit {
 		System.out.println("Hello World Git v1");
 		System.out.println("Today is June 12");
 		System.out.println("Hello Gerry");
+		System.out.println("This is version 2");
 
 	}
 
